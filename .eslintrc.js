f13f5dfbb784deddbc1d8195f34dfd9ec73e2295
Sven Greb <development@svengreb.de>
2023-05-03 22:43:25 +0200

    Adapt remaining `arcticicestudio` "fade-out" changes (#342)
    With the retirement of the "Arctic Ice Studio" personal & Nord project
    brand [1] various migrations to the `svengreb` GitHub account [2] of
    other projects were also completed within the last weeks. There are also
    changes in Nord projects like `nordtheme/web` that other projects
    depended on and can now be adapted as well.
    Therefore all Nord projects must adapt these changes which includes...
    
    - the migration and repository name change of
      `arcticicestudio/nord-docs`[2] - all references must be updated.
    - the migration of the `arcticicestudio/styleguide-git` repository to
      the `svengreb` "real-in-person" identity [3] - all references and
      used npm packages must be updated.
    - the migration of the `arcticicestudio/styleguide-javascript`
      repository to the `svengreb` "real-in-person" identity [4] - all
      references and used npm packages must be updated.
    - the migration of the `arcticicestudio/styleguide-markdown` repository
      to the `svengreb` "real-in-person" identity [5] - all references and
      used npm packages must be updated.
    
    [1]: https://github.com/orgs/nordtheme/discussions/183#retire-arctic-ice-studio-as-nord-brand
    [2]: https://github.com/nordtheme/web/issues/241
    [3]: https://github.com/svengreb/styleguide-git/issues/12
    [4]: https://github.com/svengreb/styleguide-javascript/issues/88
    [5]: https://github.com/svengreb/styleguide-markdown/issues/63
    
    GH-341

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 /*
  * Copyright (c) 2016-present Sven Greb <development@svengreb.de>
- * This source code is licensed under the MIT license found in the license file.
- * References:
+ * This source code is licensed under the Creative Commons Attribution-ShareAlike 4.0 International license found in the license file.
  */
 
 /**
@@ -9,12 +8,19 @@
  * @see https://eslint.org/docs/latest/use/configure
  * @see https://eslint.org/docs/latest/use/configure/#using-configuration-files
  * @see https://eslint.org/docs/latest/use/configure/#specifying-environments
- * @see https://eslint.org/docs/latest/use/configure/#configuring-plugins
  * @see https://eslint.org/docs/latest/rules
  */
 module.exports = {
   root: true,
-  extends: ["@arcticicestudio/eslint-config-base", "@arcticicestudio/eslint-config-base/prettier"],
+  extends: [
+    "@svengreb/eslint-config-base",
+    /*
+     * Enable support for projects using Prettier.
+     * Note that this must always be placed after the `@svengreb/eslint-config-base` preset to take precedence, otherwise it won't prevent errors
+     * due to useless and possibly conflicting rules!
+     */
+    "@svengreb/eslint-config-base/prettier",
+  ],
   overrides: [
     {
       files: ["*.js"],
